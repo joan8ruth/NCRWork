5e3cf9a86c5cca6f266d160ff8473a2de3d16aee
joan8ruth <njoanna8@gmail.com>
2019-02-08 22:32:03 +0530

    Assignment4 Q5

--- C/Assignment4/Question5/Question5/main.c
+++ C/Assignment4/Question5/Question5/main.c
@@ -1,6 +1,25 @@
 #include<stdio.h>
+#include<string.h>
 void main()
 {
-
-
+	char s[30],i,l,d;
+	printf("enter the string\n");
+	scanf("%[^\n]s", s);
+	//printf(" the string %s\n",s);
+	l = strlen(s);
+	for (i = 0; i < l; i++)
+	{
+		d = (int)s[i];
+		if (d > 64 && d < 91)
+		{
+			d = d + 32;
+			s[i] = (char)d;
+		}
+		else if (d > 96 && d < 123)
+		{
+			d = d - 32;
+			s[i] = (char)d;
+		}
+	}
+	printf(" the string %s\n", s);
 }
