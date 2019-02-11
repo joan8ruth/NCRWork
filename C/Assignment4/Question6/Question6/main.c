fc4d9eb4faf7377bc2ee1b8be10d41b628aca8c9
joan8ruth <njoanna8@gmail.com>
2019-02-11 21:31:29 +0530

    Reverse

--- C/Assignment4/Question6/Question6/main.c
+++ C/Assignment4/Question6/Question6/main.c
@@ -1,10 +1,22 @@
 #include<stdio.h>
 #include<string.h>
 
-void reverse(char* str);
+void reverse(char*);
 
 void main()
 {
-	char s[];
-
+	char a[] = "Hello";
+	reverse(a);
+	
 }
+
+void reverse(char *str)
+{
+	if (*str)
+	{
+		reverse(str + 1);
+		printf("%c", *str);
+	}
+}
+
+
