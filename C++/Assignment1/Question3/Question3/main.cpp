36c78a0e27ca7dddb66acecfa71ea742f8389ddd
joan8ruth <njoanna8@gmail.com>
2019-02-14 14:58:02 +0530

    C++

--- C++/Assignment1/Question3/Question3/main.cpp
+++ C++/Assignment1/Question3/Question3/main.cpp
@@ -24,32 +24,57 @@ class complex
 			img = i;
 		}
 
-		void add(const complex &c1,const complex &c2)
-		{
-			real = c1.real + c2.real;
-			img = c1.img + c2.img;
-
-		}
+		
 		void display()
 		{
 			cout<<"real is " <<real<<endl;
 			cout << "imaginary is" << img << endl;
 
 		}
+		friend complex add(const complex &c1, const complex &c2);
+		friend complex mul(complex &c1,complex &c2);
 };
+
+complex add(const complex &c1, const complex &c2)
+{
+	complex temp;
+	temp.real = c1.real + c2.real;
+	temp.img = c1.img + c2.img;
+	return temp;
+
+}
+
+complex mul(complex &c1,  complex &c2)
+{
+	complex temp;
+	temp.real = (c1.real * c2.real)-(c1.img*c2.img);
+	temp.img = (c1.real * c2.img)+(c1.img*c2.real);
+	return temp;
+
+}
+
 
 int main()
 {
 	complex a;
 
 	complex b(1);
-	cout << "b is" << endl;
+	cout << "b : ";
 	b.display();
+	cout << endl;
 	complex c(2, 3);
-	cout << "c is"<<endl;
+	cout << "c is ";
 	c.display();
-	a.add(b, c);
-	cout << "a is" << endl;
+	cout << endl;
+	a = add(b, c);
+	cout << "a :";
+	cout << endl;
 	a.display();
+	a = mul(b, c);
+	cout << "a : ";
+	cout << endl;
+	a.display();
+
+	return 0;
 
 }
